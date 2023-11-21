3532e9bf1aa609af184936f7059d3712c58f4ac0
Mohammed-Rifaque <cmohammedrifaque22@gmail.com>
2023-11-21 08:44:59 +0530

    ui fixes

--- src/components/common/NavBar/index.js
+++ src/components/common/NavBar/index.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import "./style.css";
 import Logo from "../../../assets/images/Main_Logo.svg";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const NavBar = () => {
   const [menu, setMenu] = useState(false);
@@ -17,11 +21,24 @@ const NavBar = () => {
             <img src={Logo} alt="logo" />
           </div>
           <div className="right-nav">
-            <span style={{ display: "flex", alignItems: "center" }} id="admin-profile">
+            <span className="admin-profile" id="admin-profile">
               <div className="menu-box">
                 <button className="hamburger-menu" onClick={handleMenu}>
                   ☰
                 </button>
+              </div>
+              <div className="search-box">
+                <SearchIcon />
+                <input type="text" placeholder="Search" style={{ backgroundColor: "transparent", border: "none", color: "white", marginLeft: "5px" }} />
+              </div>
+              <div className="notification-box">
+                <NotificationsIcon />
+              </div>
+              <div className="profile-box">
+                <AccountCircleIcon />
+              </div>
+              <div className="down-arrow-box">
+                <ArrowDropDownIcon />
               </div>
             </span>
           </div>
