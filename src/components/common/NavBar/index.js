3b2441222a624fae2aa966f9d46d9df5f2397be2
Mohammed-Rifaque <cmohammedrifaque22@gmail.com>
2023-11-21 19:26:13 +0530

    fixes

--- src/components/common/NavBar/index.js
+++ src/components/common/NavBar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
 import Logo from "../../../assets/images/Main_Logo.svg";
 import SearchIcon from "@mui/icons-material/Search";
